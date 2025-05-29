32303d8fb0f7ebc3472732d14e72ff2b04d269ce
Benjamin Smidt <ben@miruml.com>
2025-05-28 19:38:15 -0700

    chore: allow openapi regeneration with dirty openapi git repo

--- api/regen.sh
+++ api/regen.sh
@@ -3,20 +3,7 @@ set -e
 
 this_repo_root_dir=$(git rev-parse --show-toplevel)
 this_dir=$this_repo_root_dir/api
-openapi_dir=$this_dir/openapi
 gen_dir=$this_dir/gen
-
-# Check for any changes (staged, unstaged, or untracked)
-cd "$openapi_dir"
-
-echo ""
-echo "Checking for any changes (staged, unstaged, or untracked)"
-if [ -z "$(git status --porcelain)" ]; then
-    echo "There are no changes to commit"
-else
-    echo "To keep track of what openapi spec the server is using, we store the GitHub commit of the spec used. For this to be accurate, all current changes to the openapi repository must be committed. Please remove or commit your changes to the openapi repository before refreshing the openapi spec."
-    exit 1
-fi
 
 # generate the models
 cd "$this_dir"
