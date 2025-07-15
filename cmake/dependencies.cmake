0784e332300ad86ab3f834fe9fbe3801fbcbdfc3
Benjamin Smidt <ben@miruml.com>
2025-07-15 15:22:13 -0700

    fix: pthreads linker error on ubuntu 20.04

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,10 +1,14 @@
 # Use FetchContent for most dependencies
 include(FetchContent)
 
+# nlohmann/json #
+# ------------- #
 # https://github.com/nlohmann/json?tab=readme-ov-file#embedded-fetchcontent
 FetchContent_Declare(json URL https://github.com/nlohmann/json/releases/download/v3.12.0/json.tar.xz)
 FetchContent_MakeAvailable(json)
 
+# yaml-cpp #
+# --------- #
 # https://github.com/jbeder/yaml-cpp?tab=readme-ov-file#how-to-integrate-it-within-your-project-using-cmake
 FetchContent_Declare(
 yaml-cpp
@@ -13,6 +17,13 @@ GIT_TAG 0.8.0
 )
 FetchContent_MakeAvailable(yaml-cpp)
 
+# Threads #
+# ------- #
+# this should already be installed on the system (no need to fetch or otherwise import)
+find_package(Threads REQUIRED) 
+
+# Boost #
+# ------ #
 # include boost through the desired method
 if(MIRU_FETCH_BOOST)
     message(STATUS "Miru: fetching Boost with FetchContent")
